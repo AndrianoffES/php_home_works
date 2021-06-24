1621c478c667310e86c5a25602719d741f3088f0
Ivan <moonaxel@ya.ru>
2021-06-24 13:03:29 +0500

    fix_hw_5

--- fullsize.php
+++ fullsize.php
@@ -5,7 +5,7 @@ if (isset($_GET['img_id']) && is_numeric($_GET['img_id']) && $_GET['img_id']>0){
    //var_dump($_GET['img_id']);
     $img_id = $_GET['img_id'];
    //var_dump($img_id);
-$link = mysqli_connect("localhost", "root", "Andrianof65", "galary");
+$link = mysqli_connect("localhost", "root", "pass", "galary");
 if(!$link) echo mysqli_error($link) . mysqli_errno($link);
 $result = mysqli_query($link, "SELECT * FROM img_props WHERE img_id = $img_id");
 if (!$result)
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 
 <?php
-$link = mysqli_connect("mysite.local", "root", "Andrianof65", "galary");
+$link = mysqli_connect("mysite.local", "root", "pass", "galary");
 $result = mysqli_query($link,"SELECT * FROM img_props ORDER BY views desc");
 while($rows=mysqli_fetch_assoc($result)){
     $dir = 'images/'; 
