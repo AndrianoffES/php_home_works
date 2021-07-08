799ed4b93b69a1e0303ff341fd7823aee70b9477
Ivan <moonaxel@ya.ru>
2021-07-08 20:49:42 +0500

    wh8

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+define('TEMPLATES_DIR', '../views/');
+define('LAYOUTS_DIR', 'layouts/');
+define('ENGINE_DIR', '../engine/');
+
+/* DB config */
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', 'Andrianof65');
+define('DB', 'shop');
+
+//TODO попробуйте сделать эти пути абсолютными
+include "../engine/lib_autoload.php";
